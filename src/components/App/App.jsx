ede3767f0e9267ab704cd7f6bc63e8d76566bce9
fosssoft <fossmail23@gmail.com>
2024-05-18 16:23:28 +0300

    1-9

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,17 +1,33 @@
 import { useState, useEffect } from "react";
-import css from "./App.module.css";
 import ContactForm from "../ContactForm/ContactForm";
 import SearchBox from "../SearchBox/SearchBox";
 import ContactList from "../ContactList/ContactList";
-import contactsData from "../../contacts.json";
 
 export default function App() {
-  const [contacts, setContacts] = useState(() => {
-    const savedContacts = localStorage.getItem("contacts");
+  const initContacts = [
+    { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+    { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+    { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+    { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+  ];
 
-    return savedContacts !== null ? JSON.parse(savedContacts) : contactsData;
+  const [contacts, setContacts] = useState(() => {
+    const savedContacts = window.localStorage.getItem("savedCont");
+    return savedContacts ? JSON.parse(savedContacts) : initContacts;
   });
+
+  useEffect(() => {
+    window.localStorage.setItem("savedCont", JSON.stringify(contacts));
+  }, [contacts]);
+
   const [filter, setFilter] = useState("");
+
+  const filterContact = contacts.filter((contact) => {
+    return (
+      contact.name.toLowerCase().includes(filter.toLowerCase()) ||
+      contact.number.includes(filter)
+    );
+  });
 
   const addContact = (newContact) => {
     setContacts((prevContacts) => {
@@ -25,20 +41,12 @@ export default function App() {
     });
   };
 
-  const visibleContacts = contacts.filter((contact) =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
-
-  useEffect(() => {
-    localStorage.setItem("contacts", JSON.stringify(visibleContacts));
-  });
-
   return (
     <div>
-      <h1 className={css.header}>Phonebook</h1>
+      <h1>Phonebook</h1>
       <ContactForm onAdd={addContact} />
       <SearchBox value={filter} onFilter={setFilter} />
-      <ContactList contacts={visibleContacts} onDelete={deleteContact} />
+      <ContactList contacts={filterContact} onDelete={deleteContact} />
     </div>
   );
 }
